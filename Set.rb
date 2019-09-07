4a1e9b2287164132a7dea452d807920d9dd4bfb9
David <drdavidwing@gmail.com>
2019-09-07 16:07:56 -0400

    Code Review Notes and variable name replace

--- Set.rb
+++ Set.rb
@@ -2,17 +2,21 @@
 # File created 09/04/2019 by Sri Ramya Dandu
 # Edited 09/05/2019 by Leah Gillespie
 #Edited 9/06/19 David Wing
+# Edited 9/05 Neel
 
 # Created 09/05/2019 by Leah Gillespie
 # Edited 09/06/2019 by Neel Mansukhani
 # Added id and set_id to Card
 class Card
 
-    attr_reader :id, :symbol, :color, :shape, :shade
+    #TODO Attribute matching to numbers
 
-    def initialize(symbol, color, shape, shade)
+    #TODO check attributes are correct
+    attr_reader :id, :number, :color, :shape, :shade
+
+    def initialize(number, color, shape, shade)
       @id = nil
-      @symbol = symbol
+      @number = number
       @color = color
       @shape = shape
       @shade = shade
@@ -21,7 +25,7 @@ class Card
     # Cleaned up display
     def display
       print("Card: #{@id} ")
-      print("Symbol: #{@symbol} ")
+      print("number: #{@number} ")
       print("Color: #{@color} ")
       print("Shape: #{@shape} ")
       puts("Shade: #{@shade}")
@@ -34,6 +38,7 @@ end
 
 $score = 0
 $cardCount = 0
+
 # Created 09/06/2019 by Neel Mansukhani
 # Returns card from an array with the given id
 def getCardById(deck,id)
@@ -42,6 +47,7 @@ def getCardById(deck,id)
   end
   return nil
 end
+
 # Created 09/06/2019 by Neel Mansukhani
 # Removes 12 cards from deck.
 def dealCards(deck,num)
@@ -62,21 +68,24 @@ end
 # Creates an array to be the deck and initializes 81 unique cards into it
 def createDeck
   deck = Array.new
-  for symbol in 0..2
+  for number in 0..2
     for color in 0..2
       for shape in 0..2
         for shade in 0..2
-          deck.push(Card.new(symbol,color,shape,shade))
+          deck.push(Card.new(number,color,shape,shade))
         end
       end
     end
   end
   return deck
 end
+#TODO write tests for create deck
+
+
 # Created 09/04/2019 by Sri Ramya Dandu
 =begin
     Checks if the 3 cards are a valid set or not. To be a valid set, all 3 cards must either have the same
-    attribute or all different attributes for each of the following attrbitues: symbol, color,shape,shade.
+    attribute or all different attributes for each of the following attributes: number, color,shape,shade.
     @param card1, card2, card3 to evaluate whether they form a set or not
     @returns true if cards form a valid set, false otherwise
     @updates $score
@@ -84,10 +93,14 @@ end
 def isASet?(cards)
     isSet = true
 
+    #TODO optimize checks
+
+    puts("HERE #{cards[0].number}")
+
     # The sum when adding one number 3 times or adding 3 consecutive numbers is divisible by 3.
     # This represents having all the same attribute or all different attributes.
     # Adding any other 3 number combo of 1,2,3 will result in a value not divisible by 3, failing to be a set.
-    isSet = false if (cards[0].symbol + cards[1].symbol + cards[2].symbol) % 3 != 0
+    isSet = false if (cards[0].number + cards[1].number + cards[2].number) % 3 != 0
     isSet = false if (cards[0].color + cards[1].color + cards[2].color) % 3 != 0
     isSet = false if (cards[0].shape + cards[1].shape + cards[2].shape) % 3 != 0
     isSet = false if (cards[0].shade + cards[1].shade + cards[2].shade) % 3 != 0
@@ -115,6 +128,8 @@ while true # TODO: check if there are any sets left
   set = [getCardById(cardsShowing,card1),getCardById(cardsShowing,card2),getCardById(cardsShowing,card3)]
   if(isASet?(set))
     puts("That is a set!")
+    #TODO: Score should increment/decrement here
+
     #TODO: set up hash or something to clean sets up.
     sets.push(set)
     cardsShowing -= set
@@ -150,8 +165,9 @@ row3 = []
 #Created 9/06 David Wing
 #updated 9/07 David Wing
 #Check if table is valid
-def valid_table? (row1, row2, row3) #brute force check, but since there are only 12 cards to check its good enough
 
+#TODO magic number doesnt make sense
+def valid_table? (row1, row2, row3) 
   types = Hash.new(row3)
 
   for column_one in 0..row1.length
@@ -173,7 +189,7 @@ def valid_table? (row1, row2, row3) #brute force check, but since there are only
       #magic number = 6
       attrToFind[0] = 6 - card1.shape - card2.shape
       attrToFind[1] = 6 - card1.shade - card2.shade
-      attrToFind[2] = 6- card1.symbol - card2.symbol
+      attrToFind[2] = 6- card1.number - card2.number
       attrToFind[3] = 6 - card1.color - card2.color
 
       until i > 4
@@ -182,7 +198,7 @@ def valid_table? (row1, row2, row3) #brute force check, but since there are only
           
       cardToFind.shape = attrToFind[0]
       cardToFind.shade = attrToFind[1]
-      cardToFind.symbol = attrToFind[2]
+      cardToFind.number = attrToFind[2]
       cardToFind.color = attrToFind[3]
 
 
@@ -194,3 +210,4 @@ def valid_table? (row1, row2, row3) #brute force check, but since there are only
   end
   return false
 end
+end end

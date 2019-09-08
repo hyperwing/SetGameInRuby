57e70d73c9057d7ea026d6858af640bd423e3ef5
david <drdavidwing@gmail.com>
2019-09-08 15:51:35 -0400

    table check for valid sets functional
    tests written for table check

--- Set.rb
+++ Set.rb
@@ -1,39 +1,42 @@
 
 # File created 09/04/2019 by Sri Ramya Dandu
 # Edited 09/05/2019 by Leah Gillespie
-#Edited 9/06/19 David Wing
-# Edited 9/05 Neel
+# Edited 9/06/2019 David Wing
+# Edited 9/06/2019 by Neel Mansukhani
+# Edited 09/07/2019 by Sharon Qiu
+# Edited 9/07/2019 by Neel Mansukhani
+# Edited 09/07/2019 by Sri Ramya Dandu
 
 # Created 09/05/2019 by Leah Gillespie
 # Edited 09/06/2019 by Neel Mansukhani
 # Added id and set_id to Card
 class Card
 
-    #TODO Attribute matching to numbers
+  #TODO Attribute matching to numbers
 
-    #TODO check attributes are correct
-    attr_reader :id, :number, :color, :shape, :shade
+  #TODO check attributes are correct
+  attr_reader :id, :number, :color, :shape, :shade
 
-    def initialize(number, color, shape, shade)
-      @id = nil
-      @number = number
-      @color = color
-      @shape = shape
-      @shade = shade
-    end
-    # Edited 09/06/2019 by Neel Mansukhani
-    # Cleaned up display
-    def display
-      print("Card: #{@id} ")
-      print("number: #{@number} ")
-      print("Color: #{@color} ")
-      print("Shape: #{@shape} ")
-      puts("Shade: #{@shade}")
-    end
+  def initialize(number, color, shape, shade)
+    @id = nil
+    @number = number
+    @color = color
+    @shape = shape
+    @shade = shade
+  end
+  # Edited 09/06/2019 by Neel Mansukhani
+  # Cleaned up display
+  def display
+    print("Card: #{@id} ")
+    print("Number: #{@number} ")
+    print("Color: #{@color} ")
+    print("Shape: #{@shape} ")
+    puts("Shade: #{@shade}")
+  end
 
-    def set_id(num)
-      @id = num
-    end
+  def set_id(num)
+    @id = num
+  end
 end
 
 $score = 0
@@ -42,23 +45,27 @@ $cardCount = 0
 # Created 09/06/2019 by Neel Mansukhani
 # Returns card from an array with the given id
 def getCardById(deck,id)
-  deck.each do |card|
-    return card if card.id == id
-  end
-  return nil
+deck.each do |card|
+  return card if card.id == id
+end
+return nil
 end
 
 # Created 09/06/2019 by Neel Mansukhani
 # Removes 12 cards from deck.
-def dealCards(deck,num)
-  newCards = Array.new
-  for count in 0...num
-    card = deck.delete_at(rand(deck.length))
-    card.set_id($cardCount)
-    $cardCount += 1
-    newCards.push(card)
-  end
-  return newCards
+# Edited 09/07/2019 by Sharon Qiu:
+# Added in playingCards parameter. Method now updates the showing cards.
+# Added in boolean value signifying an empty deck.
+def dealCards?(deck,playingCards,num)
+emptyDeck = false
+return true if deck.length == 0
+for count in 0...num
+  card = deck.delete_at(rand(deck.length))
+  card.set_id($cardCount)
+  $cardCount += 1
+  playingCards.push(card)
+end
+return emptyDeck
 end
 
 # Created 09/05/2019 by Leah Gillespie
@@ -67,77 +74,128 @@ end
 #
 # Creates an array to be the deck and initializes 81 unique cards into it
 def createDeck
-  deck = Array.new
-  for number in 0..2
-    for color in 0..2
-      for shape in 0..2
-        for shade in 0..2
-          deck.push(Card.new(number,color,shape,shade))
-        end
+deck = Array.new
+for number in 0..2
+  for color in 0..2
+    for shape in 0..2
+      for shade in 0..2
+        deck.push(Card.new(number,color,shape,shade))
       end
     end
   end
-  return deck
+end
+return deck
 end
 #TODO write tests for create deck
 
 
 # Created 09/04/2019 by Sri Ramya Dandu
+# Edited 09/07/2019 by Sri Ramya Dandu: Optimized the checking method - made function concise
 =begin
-    Checks if the 3 cards are a valid set or not. To be a valid set, all 3 cards must either have the same
-    attribute or all different attributes for each of the following attributes: number, color,shape,shade.
-    @param card1, card2, card3 to evaluate whether they form a set or not
-    @returns true if cards form a valid set, false otherwise
-    @updates $score
+  Checks if the 3 cards are a valid set or not. To be a valid set, all 3 cards must either have the same
+  attribute or all different attributes for each of the following attributes: number, color,shape,shade.
+  @param card1, card2, card3 to evaluate whether they form a set or not
+  @returns true if cards form a valid set, false otherwise
+  @updates $score
 =end
 def isASet?(cards)
-    isSet = true
-
-    #TODO optimize checks
-
-    puts("HERE #{cards[0].number}")
-
-    # The sum when adding one number 3 times or adding 3 consecutive numbers is divisible by 3.
-    # This represents having all the same attribute or all different attributes.
-    # Adding any other 3 number combo of 1,2,3 will result in a value not divisible by 3, failing to be a set.
-    isSet = false if (cards[0].number + cards[1].number + cards[2].number) % 3 != 0
-    isSet = false if (cards[0].color + cards[1].color + cards[2].color) % 3 != 0
-    isSet = false if (cards[0].shape + cards[1].shape + cards[2].shape) % 3 != 0
-    isSet = false if (cards[0].shade + cards[1].shade + cards[2].shade) % 3 != 0
-    $score += 1 if isSet
-    return isSet
+  # The sum when adding one number 3 times or adding 3 consecutive numbers is divisible by 3.
+  # This represents having all the same attribute or all different attributes.
+  # Adding any other 3 number combo of 1,2,3 will result in a value not divisible by 3, failing to be a set.
+  isSet = (cards[0].number + cards[1].number + cards[2].number) % 3 == 0 &&
+      (cards[0].color + cards[1].color + cards[2].color) % 3 == 0 &&
+      (cards[0].shape + cards[1].shape + cards[2].shape) % 3 == 0 &&
+      (cards[0].shade + cards[1].shade + cards[2].shade) % 3 == 0
 end
 
+
+#Created 9/06/19 David Wing
+#updated 9/07-9/08 David Wing
+
+#Given an Array of Table, Check if table is valid
+# Returns Empty Array if not valid table, if valid table, returns Array of one Valid Set
+def valid_table?(tableArray) 
+
+  valid_set = Array[]
+  for card1 in 0...tableArray.length
+    for card2 in 0...tableArray.length
+      if(card1 == card2) #skip if same card
+        next
+      end
+
+      for card3 in 0...tableArray.length
+        
+        if card2 == card3 or card1 == card3 #skip if same card
+          next
+        end 
+
+        if isASet?([tableArray[card1], tableArray[card2], tableArray[card3]])
+          #found valid set
+          valid_set[0] = tableArray[card1]
+          valid_set[1] = tableArray[card2]
+          valid_set[2] = tableArray[card3]
+          break
+        end
+
+      end
+    end
+  end
+  
+  return valid_set
+end
+
+
+
+
+
 # Acts as the beginning of what would be the main method in Java
+# Edited 09/07/2019 by Neel Mansukhani
+# Testing fix
+
 #=========================== MAIN ==================================================
+if __FILE__ == $0
 deck = createDeck
 cardsShowing = Array.new
-cardsShowing += dealCards(deck,12)
+dealCards?(deck,cardsShowing,12)
 #Displays cards
 cardsShowing.each { |card| card.display }
 
 sets = Array.new
 while true # TODO: check if there are any sets left
-  # TODO: check if there are enough cards in the deck to deal.
-  print("Enter your first card number: ")
-  card1 = gets.to_i
-  print("Enter your second card number: ")
-  card2 = gets.to_i
-  print("Enter your third card number: ")
-  card3 = gets.to_i
-  set = [getCardById(cardsShowing,card1),getCardById(cardsShowing,card2),getCardById(cardsShowing,card3)]
-  if(isASet?(set))
-    puts("That is a set!")
-    #TODO: Score should increment/decrement here
 
-    #TODO: set up hash or something to clean sets up.
-    sets.push(set)
-    cardsShowing -= set
-    cardsShowing += dealCards(deck,3)
-    cardsShowing.each { |card| card.display }
-  else
-    puts("That is not a set.")
+setExists = true#valid_table?(r1,r2,r3) TODO: parameters need to be fixed
+noCardsLeft = (deck.length == 0)
+
+# Adds cards while no set exists and if there is cards left.
+while !setExists && !noCardsLeft
+  noCardsLeft = dealCards?(deck,cardsShowing,3)
+  setExists = true#valid_table?(r1,r2,r3) TODO: parameters need to be fixed
+end
+
+break if (!setExists && noCardsLeft) || (cardsShowing.length == 0)
+
+print("Enter your first card number: ")
+card1 = gets.to_i
+print("Enter your second card number: ")
+card2 = gets.to_i
+print("Enter your third card number: ")
+card3 = gets.to_i
+set = [getCardById(cardsShowing,card1),getCardById(cardsShowing,card2),getCardById(cardsShowing,card3)]
+if(isASet?(set))
+  puts("That is a set!")
+  #TODO: Score should increment/decrement here
+  score += 1
+  #TODO: set up hash or something to clean sets up.
+  sets.push(set)
+  cardsShowing -= set
+  if cardsShowing.length < 12
+    dealCards?(deck,cardsShowing,3)
   end
+  cardsShowing.each { |card| card.display }
+else
+  puts("That is not a set.")
+  score -= 1
+end
 end
 
 # Created 09/05/2019 by Leah Gillespie
@@ -149,65 +207,4 @@ end
 # puts isASet?(deck.at(1), deck.at(2), deck.at(3))
 
 
-#Created 9/06/19 David Wing
-# Create the DS for the table and set the table
-
-row1 = []
-row2 = []
-row3 = []
-
-# [][][][] + [additional cards]
-# [][][][] + []
-# [][][][] + []
-
-
-
-#Created 9/06 David Wing
-#updated 9/07 David Wing
-#Check if table is valid
-
-#TODO magic number doesnt make sense
-def valid_table? (row1, row2, row3) 
-  types = Hash.new(row3)
-
-  for column_one in 0..row1.length
-    for column_two in 0..row2.length
-
-      card1 = row1[column_one]
-      card2 = row2[column_two]
-
-      cardToFind = Card.new
-
-      attrToFind = [6,6,6,6]
-      # any two types are equal, the third must be equal
-      # any two types are different, the third must be different
-
-      #TODO add count
-
-      #TODO explain magic
-
-      #magic number = 6
-      attrToFind[0] = 6 - card1.shape - card2.shape
-      attrToFind[1] = 6 - card1.shade - card2.shade
-      attrToFind[2] = 6- card1.number - card2.number
-      attrToFind[3] = 6 - card1.color - card2.color
-
-      until i > 4
-        if attrToFind[i] ==4
-          attrToFind[i] = 1
-          
-      cardToFind.shape = attrToFind[0]
-      cardToFind.shade = attrToFind[1]
-      cardToFind.number = attrToFind[2]
-      cardToFind.color = attrToFind[3]
-
-
-      #TODO add hash
-      #if cardToFind
-
-
-    end
-  end
-  return false
 end
-end end
--- testing/Tests.rb
+++ testing/Tests.rb
@@ -1,15 +1,218 @@
 #File created 09/05/2019 by Neel Mansukhani
-#
+#Edited 09/05/2019 by Sri Ramya Dandu
+# Edited 09/07/2019 by Sri Ramya Dandu
 require_relative("../Set")
 
-  #9/06 David Wing
-  #Tests for valid sets
-  context "Check table for valid set" do
-    it "returns false when there are no valid sets" do
-      row1 = []
-      row2 = []
-      row3 = []
+# Created 09/05/2019 by Sri Ramya Dandu
+# Tests for isASet? function
+# Edited 09/07/2019 by Sri Ramya Dandu: changed the input to reflect input changes in method
+#
 
-      expect(valid_table?(row1, row2, row3)).to eq(false)
-    end
+context "All 3 cards have the same attributes for all categories " do
+
+  it "Returns true when all cards have attribute 1" do
+    card1 = Card.new(1,1,1,1)
+    card2 = Card.new(1,1,1,1)
+    card3 = Card.new(1,1,1,1)
+    set = [card1, card2, card3]
+
+    expect(isASet?(set)).to eq(true)
   end
+
+  it "Returns true when all cards have attribute 2" do
+    card1 = Card.new(2,2,2,2)
+    card2 = Card.new(2,2,2,2)
+    card3 = Card.new(2,2,2,2)
+    set = [card1, card2, card3]
+
+    expect(isASet?(set)).to eq(true)
+  end
+
+  it "Returns true when all cards have attribute 3" do
+    card1 = Card.new(0,0,0,0)
+    card2 = Card.new(0,0,0,0)
+    card3 = Card.new(0,0,0,0)
+    set = [card1, card2, card3]
+
+    expect(isASet?(set)).to eq(true)
+  end
+end
+
+context "All cards have different attributes" do
+  it "Returns true if 3 cards have the different attributes for all categories" do
+    card1 = Card.new(1,0,0,2)
+    card2 = Card.new(2,2,1,0)
+    card3 = Card.new(0,1,2,1)
+    set = [card1, card2, card3]
+
+    expect(isASet?(set)).to eq(true)
+  end
+end
+
+context "Cards are not valid sets" do
+  it "returns false when 1 attribute requirement isn't met" do
+    card1 = Card.new(1,0,1,2)
+    card2 = Card.new(2,0,1,0)
+    card3 = Card.new(0,0,2,1)
+    set = [card1, card2, card3]
+
+    expect(isASet?(set)).to eq(false)
+  end
+
+  it "returns false when 2 attribute requirements aren't met" do
+    card1 = Card.new(1,0,1,2)
+    card2 = Card.new(2,0,1,0)
+    card3 = Card.new(0,0,2,2)
+    set = [card1, card2, card3]
+
+    expect(isASet?(set)).to eq(false)
+  end
+
+  it "returns false when 3 attribute requirements aren't met" do
+    card1 = Card.new(1,0,1,2)
+    card2 = Card.new(2,1,1,0)
+    card3 = Card.new(0,0,2,2)
+    set = [card1, card2, card3]
+
+    expect(isASet?(set)).to eq(false)
+  end
+
+  it "returns false when all attribute requirements aren't met" do
+    card1 = Card.new(1,0,1,2)
+    card2 = Card.new(1,1,1,0)
+    card3 = Card.new(0,0,2,2)
+    set = [card1, card2, card3]
+
+    expect(isASet?(set)).to eq(false)
+  end
+end
+
+context "white box testing of the function to ensure each if statement works" do
+  it "returns false when the symbol attribute requirement isn't met, but all others are met" do
+    card1 = Card.new(1,1,0,2)
+    card2 = Card.new(1,1,1,2)
+    card3 = Card.new(0,1,2,2)
+    set = [card1, card2, card3]
+
+    expect(isASet?(set)).to eq(false)
+  end
+
+  it "returns false when the color attribute requirement isn't met, but all others are met" do
+    card1 = Card.new(1,1,0,2)
+    card2 = Card.new(1,2,1,2)
+    card3 = Card.new(1,1,2,2)
+    set = [card1, card2, card3]
+
+    expect(isASet?(set)).to eq(false)
+  end
+
+  it "returns false when the shape attribute requirement isn't met, but all others are met" do
+    card1 = Card.new(1,0,0,2)
+    card2 = Card.new(1,2,0,2)
+    card3 = Card.new(1,1,2,2)
+    set = [card1, card2, card3]
+
+    expect(isASet?(set)).to eq(false)
+  end
+
+  it "returns false when the shape attribute requirement isn't met, but all others are met" do
+    card1 = Card.new(1,0,0,1)
+    card2 = Card.new(1,2,0,2)
+    card3 = Card.new(1,1,0,2)
+    set = [card1, card2, card3]
+
+    expect(isASet?(set)).to eq(false)
+  end
+end
+
+
+#9/06 created by David Wing
+#9/08 updated by david
+#Tests for valid sets
+context "Check table for valid sets" do
+  it "returns empty array when there are no valid sets and only 3 cards" do
+    card1 = Card.new(1,0,0,1)
+    card2 = Card.new(1,2,0,2)
+    card3 = Card.new(1,1,0,2)    
+    tableArray = [card1, card2, card3]    
+    expected = []
+    expect(valid_table?(tableArray)).to eql(expected)
+  end
+
+  it "returns an array of valid cards when there is only a valid set left" do 
+    card1 = Card.new(0,0,0,0)
+    card2 = Card.new(0,0,0,0)
+    card3 = Card.new(0,0,0,0)
+    tableArray = [card1, card2, card3]
+
+    expected = [card1, card2, card3]
+    ret = valid_table?(tableArray)
+
+    expect(ret[0]).to have_attributes(:shape => 0, :number=>0, :shade=>0, :color=>0)
+    expect(ret[1]).to have_attributes(:shape => 0, :number=>0, :shade=>0, :color=>0)
+    expect(ret[2]).to have_attributes(:shape => 0, :number=>0, :shade=>0, :color=>0)
+  end
+
+  it "returns an empty array when there are no valid sets in a standard 12 card table" do 
+  
+    tableArray =[
+      Card.new(0,0,0,0),
+      Card.new(1,0,1,0),
+      Card.new(1,0,0,0),
+      Card.new(0,0,1,0),
+      Card.new(0,1,0,0),
+      Card.new(1,1,1,0),
+      Card.new(1,1,0,0),
+      Card.new(0,1,1,0),
+      Card.new(0,2,0,1),
+      Card.new(2,2,1,1),
+      Card.new(2,2,0,2),
+      Card.new(0,2,1,2)
+    ]
+
+    expected = []
+    expect(valid_table?(tableArray)).to eql(expected)
+  end
+
+  it "returns an valid card array when there is a valid sets in a standard 12 card table" do 
+  
+    tableArray =[
+      Card.new(0,0,0,0),
+      Card.new(1,0,1,0),
+      Card.new(1,0,0,0),
+      Card.new(0,0,1,0),
+      Card.new(0,1,0,0),
+      Card.new(1,1,1,0),
+      Card.new(1,1,0,0),
+      Card.new(0,1,1,0),
+      Card.new(0,2,0,0),
+      Card.new(2,2,1,1),
+      Card.new(2,2,0,2),
+      Card.new(0,2,1,2)
+    ]
+
+    ret = valid_table?(tableArray)
+
+    expect(ret[0]).to have_attributes(:shape => 0, :number=>0, :shade=>0, :color=>2)
+    expect(ret[1]).to have_attributes(:shape => 0, :number=>0, :shade=>0, :color=>1)
+    expect(ret[2]).to have_attributes(:shape => 0, :number=>0, :shade=>0, :color=>0)
+  end
+
+  it "returns an array of valid cards when there is a full table of cards" do 
+    deck = createDeck
+    tableArray = deck
+
+    #first set that is generated by createDeck
+    card1 = Card.new(2,2,2,2)
+    card2 = Card.new(2,2,2,1)
+    card3 = Card.new(2,2,2,4)
+
+    ret = valid_table?(tableArray)
+
+    expect(ret[0]).to have_attributes(:shape => 2, :number=>2, :shade=>2, :color=>2)
+    expect(ret[1]).to have_attributes(:shape => 2, :number=>2, :shade=>1, :color=>2)
+    expect(ret[2]).to have_attributes(:shape => 2, :number=>2, :shade=>0, :color=>2)
+  end    
+
+
+end

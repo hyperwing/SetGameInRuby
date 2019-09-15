3555ca8443e592789bd4dc188102262ff8e282df
Neel <neelm88@gmail.com>
2019-09-15 14:07:14 -0400

    Added Card file and made GameSettings changes

--- Set.rb
+++ Set.rb
@@ -18,35 +18,7 @@
 
 # TODO: Add file description for every file.
 # TODO: Multi line comments for function descriptions.
-# Created 09/05/2019 by Leah Gillespie
-# Edited 09/06/2019 by Neel Mansukhani: Added id and set_id function to Card
-# Edited 09/10/2019 by David Wing: Added id to initialize.
-# Edited 09/14/2019 by Neel Mansukhani: Added attribute accessor.
-class Card
-
-  attr_accessor :id, :number, :color, :shape, :shade
-
-  # Created 09/05/2019 by Leah Gillespie
-  # Edited 09/10/2019 by David Wing
-  def initialize(id, number, color, shape, shade)
-    @id = id
-    @number = number
-    @color = color
-    @shape = shape
-    @shade = shade
-  end
-
-  # Created 09/05/2019 by Neel Mansukhani
-  # Edited 09/06/2019 by Neel Mansukhani: Cleaned up display
-  def display
-    print("Card: #{@id} ")
-    print("Number: #{@number} ")
-    print("Color: #{@color} ")
-    print("Shape: #{@shape} ")
-    puts("Shade: #{@shade}")
-  end
-
-end
+require_relative "Card"
 
 # Created 09/12/2019 by Leah Gillespie
 class AllTimers
--- StartScreen.rb
+++ StartScreen.rb
@@ -1,19 +1,26 @@
 # File Created 09/10/2019 by Neel Mansukhani
 require 'gosu'
 require_relative 'GameSettings'
+require_relative "Card"
+
 module ZOrder
   BACKGROUND, UI, BUTTON, TEXT, CARDS= *0..4
+end
+
+module Options
+  START_SCREEN = ["SOLO", "Computer", "2 Player"]
+  LEVELS_SCREEN = ["Easy", "Medium", "Hard"]
 end
 
 GAME_TITLE = "The Game of Set"
 
 class StartScreen < Gosu::Window
 
-
   def initialize
     @game_settings = GameSettings.new
     super 840, 480
     self.caption = GAME_TITLE
+    @settings_hovered = nil
     @title_font = Gosu::Font.new(50)
     @subtitle_font = Gosu::Font.new(20)
     @background_image = Gosu::Image.new("media/background1.jpg", :tileable => true)
@@ -24,6 +31,8 @@ class StartScreen < Gosu::Window
   def update
     if Gosu.button_down? Gosu::KB_SPACE
       @game_settings.currentScreen = "game"
+    elsif Gosu.button_down Gosu::KB_ESCAPE
+      @game_settings.currentScreen = "levels"
     end
   end
 
@@ -35,32 +44,34 @@ class StartScreen < Gosu::Window
     @buttonOption.draw(190,220, ZOrder::BUTTON,0.15,0.15)
     @buttonOption.draw(360,220, ZOrder::BUTTON,0.15,0.15)
     @buttonOption.draw(530,220, ZOrder::BUTTON,0.15,0.15)
-    @subtitle_font.draw_text("SOLO", 240, 282, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
-    @subtitle_font.draw_text("Computer", 395, 282, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
-    @subtitle_font.draw_text("2 Player", 573, 282, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+    @subtitle_font.draw_text(Options::START_SCREEN[0], 240, 282, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+    @subtitle_font.draw_text(Options::START_SCREEN[1], 395, 282, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+    @subtitle_font.draw_text(Options::START_SCREEN[2], 573, 282, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
   end
 
-  def levelChoice
+  def levelsScreen
     @title_font.draw_text("Choose a level of difficulty", 170, 50, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
     @buttonOption.draw(360,90, ZOrder::BUTTON,0.15,0.15)
     @buttonOption.draw(360,170, ZOrder::BUTTON,0.15,0.15)
     @buttonOption.draw(360,250, ZOrder::BUTTON,0.15,0.15)
-    @subtitle_font.draw_text("Easy", 410, 151, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
-    @subtitle_font.draw_text("Medium", 400, 230, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
-    @subtitle_font.draw_text("Hard", 410, 310, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+    @subtitle_font.draw_text(Options::LEVELS_SCREEN[0], 410, 151, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+    @subtitle_font.draw_text(Options::LEVELS_SCREEN[1], 400, 230, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+    @subtitle_font.draw_text(Options::LEVELS_SCREEN[2], 410, 310, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
   end
   def draw
     @background_image.draw(0, 0, ZOrder::BACKGROUND)
     if @game_settings.currentScreen == "start"
       startScreen
+    elsif @game_settings.currentScreen =="levels"
+      levelsScreen
     elsif  @game_settings.currentScreen == "game"
       draw_rect(640,0,200,480,Gosu::Color::GRAY,ZOrder::UI)
       x_offset = 5
       y_offset = 35
       x_between = 90
       y_between = 135
-      for row in 0...3 #cardsPlaying.length/3
-        for col in 0...7
+      for row in 0...3
+        for col in 0...7 #cardsPlaying.length/3
           @blank_card.draw(x_offset + x_between*col,y_offset + y_between*row,ZOrder::CARDS, 0.15, 0.15)
         end
       end

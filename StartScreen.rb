95a938e74bd4db3ab22ad139416a88036cd1c5c5
Sri Ramya Dandu <dandu.7@osu.edu>
2019-09-14 20:25:24 -0400

    Worked on Gui for main menu and created window of levels of difficulty

--- StartScreen.rb
+++ StartScreen.rb
@@ -16,9 +16,9 @@ class StartScreen < Gosu::Window
     self.caption = GAME_TITLE
     @title_font = Gosu::Font.new(50)
     @subtitle_font = Gosu::Font.new(20)
-    @background_image = Gosu::Image.new("media/background.jpg", :tileable => true)
+    @background_image = Gosu::Image.new("media/background1.jpg", :tileable => true)
     @blank_card = Gosu::Image.new("media/card.png", :tileable => true)
-
+    @buttonOption = Gosu::Image.new("media/button.png", :tileable => true)
   end
 
   def update
@@ -27,12 +27,32 @@ class StartScreen < Gosu::Window
     end
   end
 
+  def startScreen
+    @title_font.draw_text("The Game of Set", 250, 50, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+    @subtitle_font.draw_text("Instructions: Select a mode of play below. The objective of the game", 160, 115, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+    @subtitle_font.draw_text("is to identify as many sets as possible. 3 cards form a set when they", 160, 145, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+    @subtitle_font.draw_text("all have the same or all different numbers, shapes, shadings, and colors.", 160, 175, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+    @buttonOption.draw(190,220, ZOrder::BUTTON,0.15,0.15)
+    @buttonOption.draw(360,220, ZOrder::BUTTON,0.15,0.15)
+    @buttonOption.draw(530,220, ZOrder::BUTTON,0.15,0.15)
+    @subtitle_font.draw_text("SOLO", 240, 282, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+    @subtitle_font.draw_text("Computer", 395, 282, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+    @subtitle_font.draw_text("2 Player", 573, 282, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+  end
+
+  def levelChoice
+    @title_font.draw_text("Choose a level of difficulty", 170, 50, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+    @buttonOption.draw(360,90, ZOrder::BUTTON,0.15,0.15)
+    @buttonOption.draw(360,170, ZOrder::BUTTON,0.15,0.15)
+    @buttonOption.draw(360,250, ZOrder::BUTTON,0.15,0.15)
+    @subtitle_font.draw_text("Easy", 410, 151, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+    @subtitle_font.draw_text("Medium", 400, 230, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+    @subtitle_font.draw_text("Hard", 410, 310, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::BLACK)
+  end
   def draw
     @background_image.draw(0, 0, ZOrder::BACKGROUND)
     if @game_settings.currentScreen == "start"
-      @title_font.draw_text("The Game of Set", 140, 50, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::WHITE)
-      @subtitle_font.draw_text("Use W, A, S, D, and Spacebar to move/select.", 130, 110, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::WHITE)
-      @subtitle_font.draw_text("Play", 270, 200, ZOrder::TEXT, 1.0, 1.0, Gosu::Color::WHITE)
+      startScreen
     elsif  @game_settings.currentScreen == "game"
       draw_rect(640,0,200,480,Gosu::Color::GRAY,ZOrder::UI)
       x_offset = 5
